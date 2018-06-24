b516ae376a1a8711b4c39a9f1c9372a57301b3a0
njcan <cantnernick@hotmail.com>
2018-06-24 15:25:59 -0400

    - Fixed orbiting bug - Commented entire ts with function and code explanations

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -35,6 +35,14 @@ export class SidebarComponent implements OnInit {
     this.startOrbit(.01);
   }
 
+  /*
+    function: getInitialPlanet()
+    inputs: none
+    outputs: none
+    purpose: This function calls the getFirstPlanet() method from the data fetcher service,
+             which will poke the backend server for the first planet's data. We then update our
+             sidebar with the information from the server
+   */
   getInitialPlanet() {
     this.dataFetcher.getFirstPlanet()
       .subscribe(planet => {
@@ -45,6 +53,14 @@ export class SidebarComponent implements OnInit {
       );
   }
 
+  /*
+  function: getNewPlanet()
+  inputs: none
+  outputs: none
+  purpose: This function calls the getNewPlanet() method from the data fetcher service,
+           which will poke the backend server for a random planet. We then update our sidebar
+           with information from the server as well as the planet's new radius and color
+ */
   getNewPlanet() {
     this.dataFetcher.getRandomPlanet()
       .subscribe(planet => {
@@ -58,84 +74,147 @@ export class SidebarComponent implements OnInit {
       );
   }
 
+  /*
+  function: startOrbit(speed)
+  inputs: speed
+  outputs: none
+  purpose: This function starts the orbit of the planet and performs orbital calculations
+ */
   startOrbit(speed: number) {
 
+    // If we are not orbiting..
     if(!this.isOrbiting) {
-      this.isOrbiting = true;
+      this.isOrbiting = true; // Set orbiting to true
     } else {
-      return;
+      return; // Else we are already orbiting, return to avoid a new orbit
     }
 
+    // Declare variables to hold rectangle coordinates for both the planet and sun
     var planetCoords = this.planet.getClientRects()[0];
     var sunCoords = document.getElementById('sun').getClientRects()[0];
 
+    // Declare variabes to hold the sun's (x,y) coordinate
     var sunX = sunCoords.left;
     var sunY = sunCoords.top;
 
+    // Declare variabes to hold the planet's (x,y) coordinate
     var planetX = planetCoords.left;
     var planetY = planetCoords.top;
 
+    // Declare variables to hold the distance between the sun and planet
     var distance = 300;
 
+    // If speed is null from updating the orbit, look to the new value set
     if(speed === null) {
       speed = Number(this.speed);
     } else {
       speed = Number(speed);
     }
 
+    // Orbiting animation control - process one frame 100 times a second for a smooth animation
     this.orbit = setInterval(doOneFrame, 10);
 
+    /*
+      function: doOneFrame()
+      inputs: none
+      outputs: none
+      purpose: This function processes one frame of the animation and updates the planet's position
+    */
     function doOneFrame() {
+
+      // 360 degrees in a circle or 2π for a full revolution
+      // If the radians doesn't exceed 2π...
       if(this.radians < (Math.PI * 2)) {
-        this.radians += speed;
+        this.radians += speed; // Continue to add speed to radians
       } else {
-        this.radians = 0;
+        this.radians = 0; // Else reset them as we have made 1 revolution
       }
-      updatePosition(this)
+      // Update our planet's position
+      updatePosition(this);
     }
 
+    /*
+      function: updatePosition(self)
+      inputs: this (itself)
+      outputs: none
+      purpose: This function calculates the new position of the planet after updating the radian value
+    */
     function updatePosition(self) {
-      var xCoord = sunX + Math.cos(self.radians) * distance;
-      var yCoord = sunY + Math.sin(self.radians) * distance;
-      self.planet.style.left = xCoord + 'px';
-      self.planet.style.top = yCoord + 'px';
+      var xCoord = sunX + Math.cos(self.radians) * distance; // x-coordinate calculation using angle and distance
+      var yCoord = sunY + Math.sin(self.radians) * distance; // y-coordinate calculation using angle and distance
+      self.planet.style.left = xCoord + 'px'; // Update x coordinate css
+      self.planet.style.top = yCoord + 'px'; // Update y coordinate css
     }
   }
 
+  /*
+    function: stopOrbit()
+    inputs: none
+    outputs: none
+    purpose: This function stops the orbit by clearing the interval that executes our animation
+  */
   stopOrbit() {
-    this.isOrbiting = false;
-    clearInterval(this.orbit);
+    this.isOrbiting = false; // Set the orbit variable to false
+    clearInterval(this.orbit); // Stop the orbit animation
   }
 
+  /*
+    function: updatePosition(self)
+    inputs: this (itself)
+    outputs: none
+    purpose: This function kicks off the planet rotation animation
+  */
   startRotation() {
-    var planet = document.getElementById('planet');
-    var speed = document.getElementById('spin-speed').value;
-    planet.style.animationDuration = (Number(speed) * 1000) + 'ms';
+    var speed = document.getElementById('spin-speed').value; // Get the initial value from the spin-speed input
+    this.planet.style.animationDuration = (Number(speed) * 1000) + 'ms'; // Update the animation duration
   }
 
+  /*
+    function: updateRotationSpeed(value)
+    inputs: value from input box
+    outputs: none
+    purpose: This function updates the rotation speed of the planet and handles validation
+  */
   updateRotationSpeed(value: number) {
+
+    // Declare variable that represent the input form
     var inputBox = document.getElementById('spin-speed');
 
-    if(isNaN(Number(value)) || Number(value) < 0) {
-      inputBox.style.border = '2px solid red';
-      this.planet.style.animationDuration = '0ms';
+    // If the value is not valid
+    if(isNaN(Number(value)) || Number(value) < 0 || String(value) === '') {
+      inputBox.style.border = '2px solid red'; // Change input box to red border
+      this.planet.style.animationDuration = '0ms'; // Stop the animation
 
+      // If the input is valid
     } else {
-      inputBox.style.border = '2px solid grey';
-      this.planet.style.animationDuration = (Number(value) * 1000) + 'ms';
+      inputBox.style.border = '2px solid grey'; // Update the input box to represent valid input
+      this.planet.style.animationDuration = (Number(value) * 1000) + 'ms'; // Update the animation to the new speed
     }
   }
 
+  /*
+    function: updateOrbitSpeed(value)
+    inputs: value from input box
+    outputs: none
+    purpose: This function updates the planet's orbit speed
+  */
   updateOrbitSpeed(value: number) {
+
+    // Declare variable for input form
     var inputBox = document.getElementById('orbit-speed');
+
+    // Coerce input to number
     this.speed = Number(value);
-    if(isNaN(Number(value))) {
-      inputBox.style.border = '2px solid red';
-      clearInterval(this.orbit);
+
+    // If the value is blank or not a number
+    if(isNaN(Number(value)) || String(value) === '') {
+      inputBox.style.border = '2px solid red'; // Set input box to red
+      this.stopOrbit(); // Stop the orbit
+
+      // If the value is valid
     } else {
-      clearInterval(this.orbit);
-      this.startOrbit(value)
+      this.stopOrbit(); // Stop the orbit
+      this.startOrbit(value); // Restart the orbit with the new value
     }
   }
-
 }
